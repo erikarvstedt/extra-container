1a4dee097e3c7fc02015866826b0e0f7aff4eea7
Erik Arvstedt <erik.arvstedt@gmail.com>
2019-11-25 15:24:00 +0100

    version 0.3

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   name = "extra-container-${version}";
-  version = "0.2";
+  version = "0.3";
 
   buildCommand = ''
     install -D ${./extra-container} $out/bin/extra-container

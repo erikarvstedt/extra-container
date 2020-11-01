1c5c788567756c4c1760e1d1710ff4140f44785e
Erik Arvstedt <erik.arvstedt@gmail.com>
2020-11-01 14:31:57 +0100

    add 'extra' container options

--- eval-config.nix
+++ eval-config.nix
@@ -96,8 +96,133 @@ let
      };
     })
   ];
+
+  containerAssert = cond: name: msg: value:
+    if cond then value
+    else throw "container '${name}': ${msg}'";
+
+  assertNonNull = var:
+    containerAssert (var != null);
+
+  extraModule = { config, pkgs, lib, ... }: with lib; {
+    options = {
+      containers = mkOption {
+        type = types.attrsOf (types.submodule (
+          { config, name, ... }: {
+            options = {
+              extra = {
+                addressPrefix = mkOption {
+                  type = with types; nullOr str;
+                  default = null;
+                  description = ''
+                    Enable privateNetwork and set
+                    hostAddress = ${addressPrefix}.1
+                    localAddress = ${addressPrefix}.2
+                  '';
+                };
+                enableWAN = mkOption {
+                  type = types.bool;
+                  default = false;
+                  description = ''
+                    Enable WAN access inside the container by rewriting container traffic
+                    to use the host's address (NAT).
+
+                    Only active when privateNetwork == true.
+                  '';
+                };
+                exposeLocalhost = mkOption {
+                  type = types.bool;
+                  default = false;
+                  description = ''
+                    Forward requests from the container's external interface
+                    to the container's localhost.
+                    Useful to test internal services from outside the container.
+
+                    WARNING: This exposes the container's localhost to all users.
+                    Only use in a trusted environment.
+
+                    Only active when privateNetwork == true.
+                  '';
+                };
+                firewallAllowHost = mkOption {
+                  type = types.bool;
+                  default = false;
+                  description = ''
+                    Always allow connections from the container host.
+
+                    Only active when privateNetwork == true.
+                  '';
+                };
+              };
+            };
+
+            config = mkMerge [
+              (
+                let
+                  prefix = config.extra.addressPrefix;
+                in mkIf (prefix != null) {
+                  privateNetwork = true;
+                  hostAddress = "${prefix}.1";
+                  localAddress = "${prefix}.2";
+                }
+              )
+              {
+                config = ({ pkgs, ... }: mkMerge [
+                  {
+                    systemd.services.forward-to-localhost = mkIf (config.extra.exposeLocalhost && config.privateNetwork) {
+                      wantedBy = [ "network.target" ];
+                      script = assertNonNull config.localAddress name
+                        ''
+                          option extra.exposeLocalhost requires localAddress to be non-null.
+                        ''
+                        ''
+                          ${pkgs.procps}/bin/sysctl -w net.ipv4.conf.all.route_localnet=1
+                          ${pkgs.iptables}/bin/iptables -t nat -I PREROUTING -p tcp \
+                            -d ${config.localAddress} ! --dport 80 -j DNAT --to-destination 127.0.0.1
+                        '';
+                    };
+                    networking.firewall.extraCommands = mkIf (config.extra.firewallAllowHost && config.privateNetwork) (
+                      assertNonNull config.hostAddress name
+                        ''
+                          option extra.exposeLocalhost requires hostAddress to be non-null.
+                        ''
+                        ''
+                          iptables -w -A nixos-fw -s ${config.hostAddress} -j ACCEPT
+                        ''
+                    );
+                  }
+                ]);
+              }
+            ];
+          }
+        ));
+      };
+    };
+
+    config = {
+      systemd.services = let
+        WANContainers = builtins.filter (c:
+                          let cfg = config.containers.${c};
+                          in cfg.privateNetwork && cfg.extra.enableWAN
+                        ) (builtins.attrNames config.containers);
+        iptables = "${pkgs.iptables}/bin/iptables";
+        serviceCfg = c: let
+          containerAddress = config.containers.${c}.localAddress;
+        in
+          assertNonNull containerAddress c
+          ''
+            option extra.enableWAN requires localAddress to be non-null
+          ''
+          {
+            preStart = "${iptables} -w -t nat -A POSTROUTING -s ${containerAddress} -j MASQUERADE";
+            postStop = "${iptables} -w -t nat -D POSTROUTING -s ${containerAddress} -j MASQUERADE || true";
+          };
+      in
+        listToAttrs (map (c: nameValuePair "container@${c}" (serviceCfg c)) WANContainers);
+    };
+  };
 in
 import "${nixos}/lib/eval-config.nix" {
   inherit baseModules;
-  modules = [ systemConfig ];
+  modules = [ extraModule systemConfig ];
 }

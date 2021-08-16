c55540a701dfa10249fac18ddaad09dd02e10a78
Erik Arvstedt <erik.arvstedt@gmail.com>
2021-08-16 18:57:42 +0200

    eval-config: add dummy option for nixpkgs unstable

--- eval-config.nix
+++ eval-config.nix
@@ -59,6 +59,7 @@ let
       users.users.systemd-coredump = dummy;
       users.users.systemd-network.uid = dummy;
       users.users.systemd-resolve.uid = dummy;
+      users.users.systemd-journal-gateway.group = dummy;
       users.users.systemd-journal-gateway.uid = dummy;
       users.groups.systemd-network.gid = dummy;
       users.groups.systemd-resolve.gid = dummy;

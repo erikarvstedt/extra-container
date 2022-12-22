013b8089600257e94650cf161492b68d79a10a0d
Erik Arvstedt <erik.arvstedt@gmail.com>
2022-12-22 11:01:16 +0100

    flake: update to NixOS 22.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Run declarative NixOS containers without full system rebuilds";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/22.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/22.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }@inputs:

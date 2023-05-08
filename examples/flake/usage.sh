46d9d7afb3d3de822217cd6515c150decf05868f
Erik Arvstedt <erik.arvstedt@gmail.com>
2023-05-08 22:43:53 +0200

    examples/flake: improve usage.sh

--- examples/flake/usage.sh
+++ examples/flake/usage.sh
@@ -1,14 +1,26 @@
 ## Usage via `nix run`
 
-# Create container defined by ./flake.nix
-nix run . -- create
+#―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+# Container lifecycle
+
+# Create and start container defined by ./flake.nix
+nix run . -- create --start
+# After changing ./flake.nix, you can also use this command to update
+# the (running) container.
+#
 # The arguments after `--` are passed to the `extra-container` binary in PATH,
 # while the flake is used for the container definitions.
+
+# Use `nixos-container` to control the running container
+sudo nixos-container run demo -- hostname
+sudo nixos-container root-login demo
 
 # Destroy container
 nix run . -- destroy
 
-# Start an interactive shell in the container
+#―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+# Container shell
+# Start an interactive shell in an ephemeral container
 nix run . -- shell
 nix run . # equivalent, because `shell` is used as the default command
 
@@ -18,13 +30,13 @@ nix run . -- --run c hostname
 nix run . -- shell --run c hostname # equivalent
 nix run . -- --run bash -c 'curl --http0.9 $ip:50'
 
-
-## Usage via `nix build`
+#―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+# Usage via `nix build`
 # 1. Build container
 nix build . --out-link /tmp/container
 # 2. Run container
 extra-container shell /tmp/container
 
-
-## Inspect container configs
+#―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+# Inspect container configs
 nix eval . --apply 'sys: sys.containers.demo.config.networking.hostName'
